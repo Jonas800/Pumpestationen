048c969ba25e6b2cc52094c549f1aaadd8a5047a
Jaqoub <33924194+Jaqoub@users.noreply.github.com>
2018-05-06 21:06:41 +0200

    jdjdj

--- src/main/java/exam/ps/MemberController.java
+++ src/main/java/exam/ps/MemberController.java
@@ -1,36 +1,40 @@
 package exam.ps;
 import java.util.*;
+
+import org.hibernate.validator.constraints.pl.REGON;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
+import java.io.*;
 @Controller
 public class MemberController {
-    ArrayList<Member> memberArray = new ArrayList<>();
+    ArrayList<Member> memberArray = getMemberArray();
     int memberID = 0;
-    public MemberController(){
+
+    public MemberController() throws FileNotFoundException {
 
     }
+
     @GetMapping("/tilføjMedlem")
-    public String Member(Model model){
+    public String Member(Model model) {
         model.addAttribute("member", new Member());
         return "tilføjMedlem";
     }
 
     @PostMapping("createMember")
-    public String createMember(@ModelAttribute Member member){
-        int ID = memberArray.size() +1;
+    public String createMember(@ModelAttribute Member member) {
+        int ID = memberArray.size() + 1;
         member.setId(ID);
         memberArray.add(member);
         return "createMember";
     }
 
     @GetMapping("/editMember")
-    public String editMember(@RequestParam(value = "ID", defaultValue = "1")int ID, Model model){
-        if(model != null) {
+    public String editMember(@RequestParam(value = "ID", defaultValue = "1") int ID, Model model) {
+        if (model != null) {
             model.addAttribute("medlem", memberArray.get((ID) - 1));
 
         }
@@ -39,36 +43,68 @@ public class MemberController {
     }
 
     @PostMapping("editMember")
-    public String editMember(@ModelAttribute Member member){
+    public String editMember(@ModelAttribute Member member) {
         member.setId(memberID);
-        memberArray.set(memberID -1, member);
+        memberArray.set(memberID - 1, member);
         return "editMember";
     }
 
     @GetMapping("deleteMember")
-    public String deleteMember(Model model){
+    public String deleteMember(@RequestParam(value = "ID", defaultValue = "0") int ID) throws FileNotFoundException {
+        memberArray.remove(ID - 1);
+        saveMemberToFile(memberArray);
+        return "redirect";
 
-        return "deleteMember";
     }
 
     @PostMapping("deleteMember")
-    public String deleteMember(@ModelAttribute Member member){
+    public String deleteMember(@ModelAttribute Member member) {
 
         return "deleteMember";
     }
-    @GetMapping("showMember")
-    public String showMember(Model model){
 
+    @GetMapping("showMember")
+    public String showMember(Model model) {
+        for (int i = 0; i < memberArray.size(); i++) {
+            model.addAttribute(memberArray.size());
+
+        }
         return "showMember";
     }
 
-    @PostMapping("showMember")
-    public String showMember(@ModelAttribute Member member){
-        for (int i = 0; i <memberArray.size() ; i++) {
+
+    public static void saveMemberToFile(ArrayList<Member> memberArray) throws FileNotFoundException {
+        PrintStream ps = new PrintStream(new File("src/main/resources/Member.txt"));
+        String s = "";
+        for (Member m : memberArray) {
+            s += m.toString();
 
         }
+        ps.print(s);
+        ps.close();
+    }
+
+    public ArrayList<Member> getMemberArray() throws FileNotFoundException {
+        ArrayList<Member> ArrayMember = new ArrayList<>();
+
+        Scanner readFile = new Scanner(new File("src/main.resources/Member.txt"));
+        while (readFile.hasNextLine()) {
+            String line = readFile.nextLine();
+            Scanner readLine = new Scanner(line).useDelimiter("#");
+
+            Member member = new Member();
+            member.setFirstName(readLine.next());
+            member.setLastName(readLine.next());
+            member.setAge(readLine.nextInt());
+            member.setCPR(readLine.next());
+            member.setId(readLine.nextInt());
+            ArrayMember.add(member);
+
+            ArrayMember.add(member);
+
+        }
+        return ArrayMember;
 
 
-        return "showMember";
     }
 }
