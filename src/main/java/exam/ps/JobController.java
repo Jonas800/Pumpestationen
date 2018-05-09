d1f211f7081e8162fc39f7adc1246793aa688da2
Jonas800 <jonasolsenit@gmail.com>
2018-05-09 09:45:00 +0200

    Database kald nu inde i JobController i stedet

--- src/main/java/exam/ps/JobController.java
+++ src/main/java/exam/ps/JobController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.sql.*;
 import java.util.ArrayList;
 
 @Controller
@@ -20,8 +21,7 @@ public class JobController {
 
         edit = editParam;
 
-        dbConn dbconn = dbConn.getInstance();
-        ArrayList<Job> allJobs = dbconn.selectAllJobs();
+        ArrayList<Job> allJobs = selectAllJobs();
         if (id != 0) {
             for (Job job : allJobs) {
                 if (job.getId() == id) {
@@ -29,7 +29,7 @@ public class JobController {
                         model.addAttribute("job", job);
                     } else if (delete) {
                         dbConn db = dbConn.getInstance();
-                        db.deleteJob(job);
+                        deleteJob(job);
 
                         return "redirect:/job";
                     }
@@ -47,10 +47,75 @@ public class JobController {
     public String createJob(@ModelAttribute Job job) {
         dbConn db = dbConn.getInstance();
         if (edit) {
-            db.updateJob(job);
+            updateJob(job);
         } else {
-            db.insertJob(job);
+            insertJob(job);
         }
         return "redirect:/job";
+    }
+
+    public ArrayList<Job> selectAllJobs(){
+        dbConn db = dbConn.getInstance();
+        Connection con = db.createConnection();
+        Statement s = null;
+        ArrayList<Job> allJobs = new ArrayList<>();
+        try{
+            s = con.createStatement();
+            ResultSet rs = s.executeQuery("SELECT * FROM jobs");
+            while(rs.next()){
+                try{
+                    Job job = new Job();
+                    job.setId(rs.getInt("job_id"));
+                    job.setTitle(rs.getString("job_title"));
+                    allJobs.add(job);
+                } catch(SQLException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
+        return allJobs;
+    }
+    public void insertJob(Job job) {
+        dbConn db = dbConn.getInstance();
+        Connection con = db.createConnection();
+        PreparedStatement ps = null;
+        try {
+            ps = con.prepareStatement("INSERT INTO jobs (job_title) VALUES(?)");
+            ps.setString(1, job.getTitle());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void updateJob(Job job) {
+        dbConn db = dbConn.getInstance();
+        Connection con = db.createConnection();
+        PreparedStatement ps = null;
+        try {
+            ps = con.prepareStatement("UPDATE jobs SET job_title = ? WHERE job_id = ?");
+            ps.setString(1, job.getTitle());
+            ps.setInt(2, job.getId());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteJob(Job job) {
+        dbConn db = dbConn.getInstance();
+        Connection con = db.createConnection();
+        PreparedStatement ps = null;
+        try {
+            ps = con.prepareStatement("DELETE FROM jobs WHERE job_id = ?");
+            ps.setInt(1, job.getId());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
