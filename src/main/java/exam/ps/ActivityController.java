34fc6b28a4ed83fda8812ff97dd1aa23c9521b5e
“YOUR <YOUR>
2018-05-11 15:45:09 +0200

    kan i finde fejlen her?

--- src/main/java/exam/ps/ActivityController.java
+++ src/main/java/exam/ps/ActivityController.java
@@ -7,67 +7,29 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.PrintStream;
 import java.sql.*;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 @Controller
 public class ActivityController {
-    ArrayList<Activity> activitiesArray = new ArrayList<>();
     int activityID = 0;
 
     @GetMapping("/visAktivitet")
     public String ShowActivities(Model model) {
-        model.addAttribute("activitiesArray", activitiesArray);
+        model.addAttribute("activityArrayList", selectAllActivities());
         return "visAktivitet";
     }
 
     @GetMapping("/tilføjAktivitet")
     public String createActivity(Model model) {
         model.addAttribute("activity", new Activity());
-
         return "tilføjAktivitet";
     }
 
     @PostMapping("/tilføjAktivitet")
-    public String createActivity(@ModelAttribute Activity activity) throws FileNotFoundException {
-        dbConn db = dbConn.getInstance();
-
+    public String createActivity(@ModelAttribute Activity activity) {
         insertActivity(activity);
-
-        return "redirect:/visAktivitet";
-    }
-
-    @GetMapping("/redigerAktivitet")
-    public String editActivity(@RequestParam(value = "id", defaultValue = "1") int id, Model model) {
-        if (model != null) {
-            model.addAttribute("activity", activitiesArray.get((id) - 1));
-        }
-        activityID = id;
-        return "redigerAktivitet";
-    }
-
-    @PostMapping("/redigerAktivitet")
-    public String editActivity(@ModelAttribute Activity activity) throws FileNotFoundException {
-        activity.setId(activityID);
-        activitiesArray.set(activityID-1, activity);
-        saveToFile(activitiesArray);
-        return "redirect:/visAktivitet";
-    }
-
-    @GetMapping("/sletAktivitet")
-    public String deleteActivity(@RequestParam(value = "id", defaultValue = "0") int id) throws FileNotFoundException {
-        for (int i = 0; i < activitiesArray.size(); i++) {
-            if (activitiesArray.get(i).getId() == id) {
-                activitiesArray.remove(i);
-            }
-        }
-        saveToFile(activitiesArray);
         return "redirect:/visAktivitet";
     }
 
@@ -88,17 +50,83 @@ public class ActivityController {
         }
     }
 
-
-
-    public static void saveToFile(ArrayList<Activity> activitiesArray) throws FileNotFoundException {
-        PrintStream ps = new PrintStream(new File("src/main/resources/templates/Activity.txt"));
-
-        String s = "";
-        for (Activity c :activitiesArray) {
-            s += c.toString() + "\r\n";
+    @GetMapping("/redigerAktivitet")
+    public String editActivity(@RequestParam(value = "id", defaultValue = "1") int id, Model model) {
+        for(Activity activity:selectAllActivities()) {
+            if(activity.getId() == id)
+                model.addAttribute("activity", activity);
         }
-        ps.print(s);
-        ps.close();
+        activityID = id;
+        return "redigerAktivitet";
+    }
 
+    public ArrayList<Activity> selectAllActivities(){
+        dbConn db = dbConn.getInstance();
+        Connection con = db.createConnection();
+        Statement s = null;
+        ArrayList<Activity> allActivities = new ArrayList<>();
+        try{
+            s = con.createStatement();
+            ResultSet rs = s.executeQuery("SELECT *  FROM activities INNER JOIN ??? ON ?? = ??? ");
+            while(rs.next()){
+                try{
+                    Activity activity = new Activity();
+                    activity.setId(rs.getInt("activity_id"));
+                    activity.setName(rs.getString("activity_name"));
+                    activity.setDescription(rs.getString("activity_description"));
+                    activity.setStartDateTime(rs.getTime("activity_startDatetime"));
+                    activity.setEndDateTime(rs.getTime("activity_endDatetime"));
+                    allActivities.add(activity);
+                } catch(SQLException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
+        return allActivities;
+    }
+
+    @PostMapping("/redigerAktivitet")
+    public String editActivity(Activity activity) {
+        updateActivity(activity);
+        return "redirect:/visAktivitet";
+    }
+
+    public void updateActivity(Activity activity) {
+        dbConn db = dbConn.getInstance();
+        Connection con = db.createConnection();
+        PreparedStatement ps = null;
+        try{
+            ps = con.prepareStatement("UPDATE activities SET activity_name = ?, activity_description = ?, activity_startDatetime = ?, activity_endDatetime = ? WHERE activity_id = ? ");
+            ps.setInt(4, activityID);
+            ps.setString(1, activity.getName());
+            ps.setString(2, activity.getDescription());
+            ps.setDate(3, new java.sql.Date(activity.getStartDateTime().getTime()));
+            ps.setDate(4, new java.sql.Date(activity.getEndDateTime().getTime()));
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @GetMapping("/sletAktivitet")
+    public String deleteActivity(@RequestParam(value = "id", defaultValue = "0") int id, Activity activity) throws FileNotFoundException {
+        deleteActivity(activity);
+        return "redirect:/visAktivitet";
+    }
+
+    public void deleteActivity(Activity activity) {
+        dbConn db = dbConn.getInstance();
+        Connection con = db.createConnection();
+        PreparedStatement ps = null;
+        try {
+            ps = con.prepareStatement("DELETE FROM activities WHERE activity_id = ?");
+            ps.setInt(1, activity.getId());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
